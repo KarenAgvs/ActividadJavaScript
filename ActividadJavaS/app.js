1a3dcd27211efbe9e632d44025c6269880ef375a
unknown <kdagudelo70@misena.edu.co>
2021-10-07 00:44:54 +0200

    Se agrega la funcion de validar al proyecto

--- ActividadJavaS/app.js
+++ ActividadJavaS/app.js
@@ -1,10 +1,20 @@
 const nombreUsuario= document.querySelector("#txtNombre")
-const edadUusuario= document.querySelector("#txtEdad")
+const edadUsuario= document.querySelector("#txtEdad")
 const boton= document.querySelector("#botonVotar")
 const mensaje= document.querySelector("#Mensaje")
 
 boton.addEventListener('click', ValidarEdad) 
 
 function ValidarEdad (){
+    const Edad=parseInt(edadUsuario.value)
+    if (Edad >=18) {
+        mensaje.textContent=" ${nombreUusario.value} su voto es permitido"
+        
+    }
+    if (Edad <18){
+        mensaje.textContent=" ${nombreUusario.value} su voto no es permitido"
+    }
+    if(Edad <=0)
+    mensaje.textContent= "Edad inválida"
 
 }
