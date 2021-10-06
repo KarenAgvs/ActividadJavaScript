43aa3a81402c10476acb492f83622b1fb72a84ba
unknown <kdagudelo70@misena.edu.co>
2021-10-07 00:26:10 +0200

    Inicio de la actividad

---
+++ ActividadJavaS/app.js
@@ -0,0 +1,10 @@
+const nombreUsuario= document.querySelector("#txtNombre")
+const edadUusuario= document.querySelector("#txtEdad")
+const boton= document.querySelector("#botonVotar")
+const mensaje= document.querySelector("#Mensaje")
+
+boton.addEventListener('click', ValidarEdad) 
+
+function ValidarEdad (){
+
+}
