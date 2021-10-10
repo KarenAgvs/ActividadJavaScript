46b527db3a0382af26fe261c7b9204e1ea773278
unknown <kdagudelo70@misena.edu.co>
2021-10-10 15:25:17 +0200

    Se termina el proceso logico

--- ActividadJavaS/app.js
+++ ActividadJavaS/app.js
@@ -8,13 +8,14 @@ boton.addEventListener('click', ValidarEdad)
 function ValidarEdad (){
     const Edad=parseInt(edadUsuario.value)
     if (Edad >=18) {
-        mensaje.textContent=" ${nombreUusario.value} su voto es permitido"
+        mensaje.textContent=` ${nombreUsuario.value} su voto es permitido`
         
     }
-    if (Edad <18){
-        mensaje.textContent=" ${nombreUusario.value} su voto no es permitido"
+    else if (Edad <18){
+        mensaje.textContent=` ${nombreUsuario.value} su voto no es permitido`
     }
     if(Edad <=0)
     mensaje.textContent= "Edad inválida"
 
 }
+console.log('hola');
